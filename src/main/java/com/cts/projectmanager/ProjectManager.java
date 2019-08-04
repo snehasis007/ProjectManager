8aef20c6cd7e905daf872c5284b749b0add89b4c
snehasis007 <snehasis007@gmail.com>
2019-08-04 23:28:36 +0530

    up

--- src/main/java/com/cts/projectmanager/ProjectManager.java
+++ src/main/java/com/cts/projectmanager/ProjectManager.java
@@ -2,11 +2,17 @@ package com.cts.projectmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class ProjectManager {
+public class ProjectManager extends SpringBootServletInitializer{
 	public static void main(String[] args) {
         SpringApplication.run(ProjectManager.class, args);
+    }
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProjectManager.class);
     }
 }
